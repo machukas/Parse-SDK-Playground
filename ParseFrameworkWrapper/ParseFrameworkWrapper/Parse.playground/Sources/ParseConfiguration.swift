497004ccff1ab1d853a5b9b68a3df3f184314aa8
machukas <machukkas@gmail.com>
2018-07-18 14:29:51 +0200

    Fix: ParseConfiguration Public Init

--- ParseFrameworkWrapper/ParseFrameworkWrapper/Parse.playground/Sources/ParseConfiguration.swift
+++ ParseFrameworkWrapper/ParseFrameworkWrapper/Parse.playground/Sources/ParseConfiguration.swift
@@ -1,5 +1,11 @@
 public struct ParseConfiguration {
 	
+	public init(server_url: String, client_key: String, app_id: String) {
+		self.server_url = server_url
+		self.client_key = client_key
+		self.app_id = app_id
+	}
+	
 	public let server_url: String
 	public let client_key: String
 	public let app_id: String
